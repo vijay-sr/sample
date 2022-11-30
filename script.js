6e988cc7646e6f60af0e5a50b8c3f5e15d324592
vijay-sr <vijayswaminathan.r@gmail.com>
2022-11-30 17:46:45 +0530

    signup form added

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 var http=require('http')
 var fs=require('fs')
+var url=require('url')
+const { log } = require('console')
+
 http.createServer(serv).listen(7000, ()=>console.log("server started"))
 function serv(req,res){
-    if(req.url==='/'){
-    fs.readFile(index.html,function(err,dat){
+    var newurl=url.parse(req.url,true)
+    // console.log(newurl.pathname);
+
+    if(newurl.pathname==='/'){
+    fs.readFile('index.html',function(err,dat){
         res.writeHead(200,{'Content-Type':'text/html'})
         res.write(dat)
         res.end()
     })
-}else if(req.url==='/login'){
-    fs.readFile(signup.html ,function(err,login){
-    
-    res.write(login)
+}else if(newurl.pathname==='/signup'){
+    fs.readFile('signup.html' ,function(err,loginpage){
+        res.writeHead(200,{'Content-Type':'text/html'})
+    res.write(loginpage)
     res.end()
 })
+}
+else if(newurl.pathname==='/signupaction'){
+    // fs.readFile()
+    res.writeHead(200,{'Content-Type':'text/html'})
+    // console.log(newurl.query);
+    res.write('<h1>'+ newurl.query.name +'</h1>')
+    res.end()
+
 }
 else{
     res.writeHead(404,{'Content-Type':'text/html'})
